a9ef04b0462819f03a318d6027ecd8f5d13bbea2
Zachary Schneider <schneider@boundary.com>
2015-04-20 14:07:59 -0500

    fix event fire uri

--- lib/cascade/event.rb
+++ lib/cascade/event.rb
@@ -5,7 +5,7 @@ module Cascade
   module Event
     def self.fire(event)
       begin
-        response = HTTParty.put(Cascade.uri + '/v1/event/' + event.name, body: event.to_json, headers: 'Content-Type' => 'application/json', timeout: 15)
+        response = HTTParty.put(Cascade.uri + '/v1/event/fire/' + event.name, body: event.to_json, headers: {'Content-Type' => 'application/json'}, timeout: 15)
         JSON.parse(response.body)
       rescue
         {}
