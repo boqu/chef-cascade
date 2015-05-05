bcbfb9ec05789c3c67f013fe8e55a7f313362498
Zachary Schneider <schneider@boundary.com>
2015-05-05 13:11:51 -0500

    add service registration

--- lib/cascade/service.rb
+++ lib/cascade/service.rb
@@ -11,5 +11,20 @@ module Cascade
         []
       end
     end
+
+    def self.register(name, type, port)
+      begin
+        service = {
+          'ID' => "#{name}_#{type}",
+          'Name' => name,
+          'Tags' => [type]
+        }  
+
+        response = HTTParty.put(Cascade.uri + '/v1/agent/service/register', body: service.to_json, headers: {'Content-Type' => 'application/json'}, timeout: 15)
+        true
+      rescue
+        false
+      end
+    end
   end
 end
