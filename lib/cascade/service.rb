47c530b9068e4a107954a5edf46f3e84fbbc02c5
Zachary Schneider <schneider@boundary.com>
2015-05-05 14:52:17 -0500

    add more service methods

--- lib/cascade/service.rb
+++ lib/cascade/service.rb
@@ -3,9 +3,9 @@ require 'json'
 
 module Cascade
   module Service
-    def self.find(service)
+    def self.find(service, type='service')
       begin
-        response = HTTParty.get(Cascade.uri + '/v1/catalog/service/' + service, timeout: 15)
+        response = HTTParty.get(Cascade.uri + '/v1/catalog/service/' + service + '?tag=' + type, timeout: 15)
         JSON.parse(response.body).map {|service| {ip: service['Address'], port: service['ServicePort']} }
       rescue
         []
@@ -21,6 +21,24 @@ module Cascade
         }  
 
         response = HTTParty.put(Cascade.uri + '/v1/agent/service/register', body: service.to_json, headers: {'Content-Type' => 'application/json'}, timeout: 15)
+        true
+      rescue
+        false
+      end
+    end
+
+    def self.registered?(name, type)
+      begin
+        response = HTTParty.get(Cascade.uri + '/v1/agent/services', body: service.to_json, headers: {'Content-Type' => 'application/json'}, timeout: 15)
+        return (JSON.parse(response)["#{name}_#{type}"]['Service'] == name) ? true : false
+      rescue
+        false
+      end
+    end
+
+    def self.deregister(name, type)
+      begin
+        response = HTTParty.get(Cascade.uri + '/v1/agent/service/deregister/' + "#{name}_#{type}", body: service.to_json, headers: {'Content-Type' => 'application/json'}, timeout: 15)
         true
       rescue
         false
