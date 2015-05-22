1f645cbcb78e31f3e549caf62e2daa0a879ab246
Zachary Schneider <schneider@boundary.com>
2015-05-22 14:47:20 -0500

    fix kv

--- lib/cascade/key_value.rb
+++ lib/cascade/key_value.rb
@@ -7,9 +7,9 @@ module Cascade
   module KeyValue
     def self.get(key)
       begin
-        response = HTTParty.get(Cascade.uri + '/v1/kv/' + key, timeout: 15)
+        response = HTTParty.get(Cascade.uri + '/v1/kv' + key, timeout: 15)
         
-        YAML.load(Base64.decode64(JSON.parse(response.body)['Value']))
+        YAML.load(Base64.decode64(JSON.parse(response.body).first['Value']))
       rescue
         {}
       end
