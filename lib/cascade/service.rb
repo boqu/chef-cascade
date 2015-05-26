235b1ae1ab54eda2d0bbbdd9a44c11d8182289c2
Zachary Schneider <schneider@boundary.com>
2015-05-26 11:55:56 -0500

    standardize naming

--- lib/cascade/service.rb
+++ lib/cascade/service.rb
@@ -3,9 +3,9 @@ require 'json'
 
 module Cascade
   module Service
-    def self.find(service, type='service')
+    def self.find(service, tag='service')
       begin
-        response = HTTParty.get(Cascade.uri + '/v1/catalog/service/' + service + '?tag=' + type, timeout: 15)
+        response = HTTParty.get(Cascade.uri + '/v1/catalog/service/' + service + '?tag=' + tag, timeout: 15)
         JSON.parse(response.body).map {|service| {ip: service['Address'], port: service['ServicePort']} }
       rescue
         []
