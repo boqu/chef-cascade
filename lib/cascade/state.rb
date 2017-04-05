b09e25d9cd9822d8a67dfd3d8cbd909b9be7a576
Zachary Schneider <sigil.66@gmail.com>
2017-04-05 12:24:42 -0500

    add basic state management

--- lib/cascade/state.rb
+++ lib/cascade/state.rb
@@ -1,5 +1,50 @@
+require 'yaml'
+
 module Cascade
   module State
     STATE_DIR = '/var/chef/cascade/state'
+  
+    def self.get(namespace, key)
+      begin
+        state_file = ::File.join(STATE_DIR, namespace+".yaml")
+        return YAML.load_file(state_file)[key]
+      rescue
+        return nil
+      end
+    end
+
+    def self.set(namespace, key, value)
+      begin
+        state_file = ::File.join(STATE_DIR, namespace+".yaml")
+        state = YAML.load_file(state_file)
+
+        state[key] = value
+        File.open(state_file, 'w') do |f| 
+          f.write state.to_yaml
+        end
+        
+        return true
+      rescue
+        return false
+      end
+    end
+
+    def self.set_once(namespace, key, value)
+      begin
+        state_file = ::File.join(STATE_DIR, namespace+".yaml")
+        state = YAML.load_file(state_file)
+
+        if state[key] == nil
+          state[key] = value
+          File.open(state_file, 'w') do |f| 
+            f.write state.to_yaml
+          end
+        end
+        
+        return state[key]
+      rescue
+        return false
+      end
+    end
   end
 end
